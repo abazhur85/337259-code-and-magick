200874b13f3f7650f1057eb444c25b0142c5e56d
abazhur85 <abazhur85@gmail.com>
2017-04-01 12:39:44 -0400

    Lesson 2.1

--- js/stat.js
+++ js/stat.js
@@ -1,14 +1,14 @@
-‘use strict’;
+'use strict';
 
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
 var names = {
-name1: ‘Марта’,
-name2: ‘Вы’,
-name3: ‘Арам’
+name1: 'Марта',
+name2: 'Вы',
+name3: 'Арам'
 } ;
-var times = [ time1, time2, time3];
+var times = [ time1, time2, time3 ];
 
 var max = -1;
 var maxIndex = -1;
@@ -28,8 +28,8 @@ var window = {
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
 
-  ctx.fillText (‘Ура, вы победили!’, 120, 40);
-  ctx.fillText (‘Список результатов:’, 120, 55);
+  ctx.fillText ('Ура, вы победили!', 120, 40);
+  ctx.fillText ('Список результатов:', 120, 40);
 
   for (var i = 0 ; i < times.length; i++) {
     var time = times[i];
@@ -43,10 +43,10 @@ var window = {
 
   ctx.textBaseline = 'bottom';
 
-  names[‘Вы’] ? ctx.fillStyle = 'rgba(255, 0, 0, 1)': 'rgb(0, 112, 180)'
+  names['Вы'] ? ctx.fillStyle = 'rgba(255, 0, 0, 1)': 'rgb(0, 112, 180)'
 
   for (var i = 0; i < times.length; i++) {
     ctx.fillRect(120+ 90 * i, 250 - times[i] * step, 40, times[i] * step);
     ctx.fillText(names[i], 120 + 90 * i, 270);
   }
-}
+  }
