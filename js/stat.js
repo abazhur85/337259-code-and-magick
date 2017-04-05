ae7dd61378bf36d551fce5706cd6616243139620
abazhur85 <abazhur85@gmail.com>
2017-04-05 13:31:01 -0400

    LESSON 2.3

--- js/stat.js
+++ js/stat.js
@@ -3,22 +3,19 @@ var max = -1;
 var maxIndex = -1;
 
 window.renderStatistics = function (ctx, names, times) {
-  return;
+
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(120, 25, 420, 270);
 
   ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
   ctx.fillRect(100, 10, 420, 270);
   ctx.strokeRect(100, 10, 420, 270);
 
-
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(120, 25, 420, 270);
-
-
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
 
   ctx.fillText('Ура, вы победили!', 120, 40);
-  ctx.fillText('Список результатов:', 120, 40);
+  ctx.fillText('Список результатов:', 120, 60);
 
   for (var i = 0; i < times.length; i++) {
     var time = times[i];
